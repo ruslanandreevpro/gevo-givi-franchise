9acc1630c09a1c67910a82c94d1ddb93e6d8d69c
Ruslan Andreev <ruslanandreevpro@gmail.com>
2019-09-13 14:15:57 +0400

    add geolocation

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,35 @@ import "./import/modules";
 import "./import/components";
 import "abpetkov-powerange/dist/powerange.min";
 import "instafeed.js";
+import "geolocator";
+
+document.addEventListener("DOMContentLoaded", event => {
+    document.getElementById("sidenav").setAttribute("style", "height: calc(100vh - 50px)");
+    // document.getElementById("sidenav").setAttribute("style", "height: " + window.screen.availHeight + "px");
+});
+
+let geolocator = require("geolocator");
+
+geolocator.config({
+    language: "ru",
+    google: {
+        version: "3",
+        key: "AIzaSyBA5worfgANjXL7i8y1dgiQa55o7cXq49A"
+    }
+});
+
+window.onload = () => {
+    let options = {
+        enableHighAccuracy: true,
+        timeout: 0,
+        maximumWait: 0,         // max wait time for desired accuracy
+        maximumAge: 0,          // disable cache
+        desiredAccuracy: 100,    // meters
+        fallbackToIP: true,     // fallback to IP if Geolocation fails or rejected
+        addressLookup: true,    // requires Google API key if true
+    };
+    geolocator.locate(options, function (err, location) {
+        if (err) return console.log(err);
+        document.getElementById("town-3").innerHTML = "<p>" + location.address.city + "</p>";
+    });
+};
